87164bfc4bed2217c25f4f1fd759da3f8631129b
laozhupeiqi <125838081+laozhupeiqia@users.noreply.github.com>
2024-08-11 11:22:45 +0800

    Add files via upload

---
+++ wanzhou.js
@@ -0,0 +1,16 @@
+ 
+ // 介绍部分
+ $(".jss2").hide(1)
+$(".jss3").hide(1)
+$("#js1").hover(function(){
+			$(".jss1").show(1)
+			$(".jss2,.jss3").hide(1)
+				})
+$("#js2").hover(function(){
+			$(".jss2").show(1)
+			$(".jss1,.jss3").hide(1)
+				})
+$("#js3").hover(function(){
+			$(".jss3").show(1)
+			$(".jss1,.jss2").hide(1)
+				})	
